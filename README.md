48310a104ca3fef38d91518ee313304050b3f5b4
Israel Boudoux <iboudoux@polygon.technology>
2023-09-20 16:38:19 +0100

    Adding README info

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # cryptography-playground
+
+In this playground I'll try to add some resources related to Cryptography. Mostly written in Java, however other times it 
+might be written in other languages like Typescript, Golang or Rust (if I have disposition for this :)).
+
+## Resources
+# Schoolbook RSA
+Basically a Schoolbook RSA is an implementation that follows the steps to encrypt/decrypt data, but suffers from some vulnerabilities. Without applying padding, the algorithm may fall short against some attacks, like the following:
+If public exponents (_e_) and Plain Texts are small, raising the Plain Text by _e_ might not hit the modulo operation, which would allow an attacker to recover the Plain Text by applying the _e_-th root on the Ciphertext.
+Yet, even using padding, RSA may still suffer from [Chosen-Ciphertext Attacks (CCA)](https://en.wikipedia.org/wiki/Chosen-ciphertext_attack). There are two kinds of CCA attacks:
+_CCA1_ - passive chosen ciphertext attack; _CCA2_ - adaptative Chosen Ciphertext. For example, the PKCS#1v1.5 (Public Key Cryptography Standard) specifies a padding that may not withstand against
+CC2 attacks.
+
+The resources related to this topic are under: 'main/java/src/edu/boudoux/rsa'
+Tests for the resources related to this topic are under: 'test/java/src/edu/boudoux/rsa'
+
+One can play with RSA by running the class 'main/java/src/edu/boudoux/rsa/SchoolBookRsa.java'. 
+
+Probably bugs are around. Let me know by raising an issue!
--- src/main/java/edu/boudoux/rsa/SchoolBookRSA.java
+++ src/main/java/edu/boudoux/rsa/SchoolBookRSA.java
@@ -22,10 +22,20 @@ public class SchoolBookRSA {
 
     /**
      * This is the default 'e' selected, but it could be any prime number that satisfies
-     * gcd(e, p-1) = 1 and gcd(e, q-1) = 1.
+     * gcd(e, p-1) = 1 and gcd(e, q-1) = 1. A good thing is to use a number that has a small
+     * amount of bits set, like 17 or 65537.
      */
     public static final BigInteger DEFAULT_E = new BigInteger("17");
 
+    /**
+     * Generates a prime that satisfies that has half of the bits in 'totalBits' && is coprime with 'e' && different from
+     * otherThan.
+     *
+     * @param totalBits the total bits in the modulo
+     * @param e the public component 'e'
+     * @param otherThan a number that the number being generated should be different from. It can be null
+     * @return
+     */
     public static BigInteger generatePrime(int totalBits, BigInteger e, BigInteger otherThan) {
         /*
            In order to generate a number lower than the total bits, one is subtracted from the total, since it will
@@ -75,12 +85,12 @@ public class SchoolBookRSA {
         return Map.entry(new PubKey(e, n), new PrivKey(d, n));
     }
 
-    private static boolean validateKeySize(int keySize) {
-        return keySize >= 8 && keySize <= 1024 && keySize % 8 == 0;
+    private static boolean validateKeySize(int moduloSize) {
+        return moduloSize >= 8 && moduloSize <= 1024 && moduloSize % 8 == 0;
     }
 
-    private static boolean validatePlainText(String v, int keySize) {
-        int maxPlainTextLength = keySize / 8;
+    private static boolean validatePlainText(String v, int moduloSize) {
+        int maxPlainTextLength = moduloSize / 8;
         return !v.isEmpty() && v.getBytes(StandardCharsets.UTF_8).length <= maxPlainTextLength;
     }
 
@@ -92,14 +102,32 @@ public class SchoolBookRSA {
         return CryptographyUtils.mmi2(e, totientN);
     }
 
+    /**
+     * Encrypts the plain text
+     *
+     * @param plainTextInput the plain text to encrypt
+     * @param e the public component 'e'
+     * @param n the public component 'n'
+     *
+     * @return
+     */
     public static BigInteger cipherPlainText(String plainTextInput, BigInteger e, BigInteger n) {
         BigInteger text = CryptographyUtils.toBigInteger(plainTextInput);
 
         return CryptographyUtils.powerMod(text, e, n);
     }
 
-    public static String decipher(BigInteger cypheredText, BigInteger privateKey, BigInteger n) {
-        BigInteger plainTextRep = CryptographyUtils.powerMod(cypheredText, privateKey, n);
+    /**
+     * Decrypts the cipher text
+     *
+     * @param cipheredText the ciphered text
+     * @param privateKey the private component 'd'
+     * @param n the public component 'n'
+     *
+     * @return the plain text being represented by the cipher text, if all the params are correct
+     */
+    public static String decipher(BigInteger cipheredText, BigInteger privateKey, BigInteger n) {
+        BigInteger plainTextRep = CryptographyUtils.powerMod(cipheredText, privateKey, n);
         return CryptographyUtils.toString(plainTextRep, n);
     }
 
@@ -134,8 +162,9 @@ public class SchoolBookRSA {
         System.out.printf("Ciphered text: %s (int rep: %s)\n\n", cipheredText, cipheredTextRep);
 
         System.out.println("Deciphering...");
+
         // we will assume 'n' is already supplied, and the cipher text as well
-        String privateKey = requestInput("\nEnter the private key (the 'd' component): ",
+        String privateKey = requestInput("\nEnter the private key (copy the 'd' component): ",
                 (String s) -> !s.isEmpty() && n.compareTo(BigInteger.valueOf(s.length())) > 0);
         String plainText = decipher(cipheredTextRep, new BigInteger(privateKey), n);
 
