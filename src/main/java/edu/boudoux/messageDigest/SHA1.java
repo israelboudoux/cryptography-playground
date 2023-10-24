42efaeb9d678788221f1c2ca61449973bb2f9594
Israel Boudoux <iboudoux@polygon.technology>
2023-10-24 15:53:07 +0100

    Adding SHA-1 Message Digest from 'https://github.com/israelboudoux/md4-sha'

---
+++ src/main/java/edu/boudoux/messageDigest/SHA1.java
@@ -0,0 +1,187 @@
+package edu.boudoux.messageDigest;
+
+public class SHA1 {
+
+    /**
+     * The initial 160bits hash
+     */
+    private static final int[] H_0 = new int[] {
+            0x67452301,
+            0xefcdab89,
+            0x98badcfe,
+            0x10325476,
+            0xc3d2e1f0
+    };
+
+    /**
+     * The constants applied in each of the four stages.
+     * Each stage is represented by an index in this array.
+     */
+    private static final int[] K = new int[] {
+            0x5a827999,
+            0x6ed9eba1,
+            0x8f1bbcdc,
+            0xca62c1d6
+    };
+
+    public static String hash(byte[] input) {
+        if (input == null) return null;
+
+        int[] paddedContent = applyPadding(input);
+        int[][] blocks = parseMessage(paddedContent);
+
+        int[] currentHash = H_0;
+        int[] dividedBlock;
+
+        for (int[] block: blocks) {
+            dividedBlock = divideBlock(block);
+            currentHash = processBlock(dividedBlock, currentHash);
+        }
+
+        return toString(currentHash);
+    }
+
+    static int[][] parseMessage(int[] paddedContent) {
+        int[][] block = new int[paddedContent.length / 64][64];
+
+        for (int i = 0; i < block.length; i++) {
+            System.arraycopy(paddedContent, i * 64, block[i], 0, 64);
+        }
+
+        return block;
+    }
+
+    static int[] processBlock(int[] dividedBlock, int[] previousHash) {
+        int[] word = new int[80];
+
+        int a = previousHash[0];
+        int b = previousHash[1];
+        int c = previousHash[2];
+        int d = previousHash[3];
+        int e = previousHash[4];
+
+        int T;
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = i * 20; j < 20 + i * 20; j++) {
+                word[j] = messageSchedule(dividedBlock, j, word);
+
+                T = rotl(a, 5) + applyF(b, c, d, i) + e + K[i] + word[j];
+                e = d;
+                d = c;
+                c = rotl(b, 30);
+                b = a;
+                a = T;
+            }
+        }
+
+        a = a + previousHash[0];
+        b = b + previousHash[1];
+        c = c + previousHash[2];
+        d = d + previousHash[3];
+        e = e + previousHash[4];
+
+        return new int[] {a, b, c, d, e};
+    }
+
+    static int messageSchedule(int[] dividedBlock, int j, int[] word) {
+        if (j < 16)
+            return dividedBlock[j];
+
+        return rotl(word[j - 3] ^ word[j - 8] ^ word[j - 14] ^ word[j - 16], 1);
+    }
+
+    static int[] divideBlock(int[] block) {
+        int[] result = new int[16];
+
+        for (int mainIndex = 0, blockIndex = 0; mainIndex < 16; mainIndex++, blockIndex += 4) {
+            long value = block[blockIndex] & 0xff;
+            value <<= 8;
+            value |= block[blockIndex + 1] & 0xff;
+            value <<= 8;
+            value |= block[blockIndex + 2] & 0xff;
+            value <<= 8;
+            value |= block[blockIndex + 3] & 0xff;
+
+            result[mainIndex] = (int) value;
+        }
+
+        return result;
+    }
+
+    static int[] getPaddingBytes(long contentLength) {
+        long kBits = mod(448L - (contentLength * 8L + 1L), 512L);
+        long paddingSizeInBits = 1 + kBits + 64;
+        int arrSize = (int) (paddingSizeInBits / 8 + (paddingSizeInBits % 8 == 0 ? 0 : 1));
+        int[] padding = new int[arrSize];
+
+        // padding = bit 1 + 0 kBits + contentLength total bits in 64bits representation
+
+        // fill the initial positions: bit 1 + kBits bit 0 (MSB)
+        padding[0] = 0x80;
+
+        // fill the 64 (LSB) - contentLength total bits in 64bits representation
+        int offset = arrSize - 8;
+        long bitsInContentLength = contentLength * 8;
+        double calc, sum = 0;
+        for (int i = 1, arrIndex = offset, power = 63; i <= 8; i++, arrIndex++) {
+            for (int j = 1; j <= 8; j++) {
+                padding[arrIndex] = padding[arrIndex] << 1;
+
+                calc = Math.pow(2, power--);
+                if (sum + calc <= bitsInContentLength) {
+                    sum += calc;
+                    padding[arrIndex] = (padding[arrIndex] | 0x01);
+                }
+            }
+        }
+
+        return padding;
+    }
+
+    static int[] applyPadding(byte[] value) {
+        int contentLength = value.length;
+        int[] padding = getPaddingBytes(contentLength);
+
+        int[] result = new int[contentLength + padding.length];
+        for (int i = 0; i < contentLength; i++)
+            result[i] = value[i];
+
+        System.arraycopy(padding, 0, result, contentLength, padding.length);
+
+        return result;
+    }
+
+    static int rotl(int value, int n) {
+        return value << n | value >>> (32 - n);
+    }
+
+    static int applyF(int b, int c, int d, int stageIndex) {
+        if (stageIndex == 0)
+            return (b & c) ^ (~b & d);
+        else if (stageIndex == 1)
+            return b ^ c ^ d;
+        else if (stageIndex == 2)
+            return (b & c) ^ (b & d) ^ (c & d);
+
+        return b ^ c ^ d;
+    }
+
+    static String toString(int[] currentHash) {
+        return hex32Pad(Integer.toHexString(currentHash[0])) +
+                hex32Pad(Integer.toHexString(currentHash[1])) +
+                hex32Pad(Integer.toHexString(currentHash[2])) +
+                hex32Pad(Integer.toHexString(currentHash[3])) +
+                hex32Pad(Integer.toHexString(currentHash[4]));
+    }
+
+    static String hex32Pad(String value) {
+        return String.format("%8s", value).replace(" ", "0");
+    }
+
+    private static long mod(long value, long modValue) {
+        if (value >= 0) return value % modValue;
+
+        return (-(-value % modValue) + modValue) % modValue;
+    }
+}
---
+++ src/test/java/edu/boudoux/messageDigest/SHA1NistTests.java
@@ -0,0 +1,100 @@
+package edu.boudoux.messageDigest;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+@RunWith(Parameterized.class)
+public class SHA1NistTests {
+
+    private final TestCase testCase;
+
+    public SHA1NistTests(TestCase testCase) {
+        this.testCase = testCase;
+    }
+
+    private static class TestCase {
+        public byte[] value;
+        public String result;
+        public TestCase(byte[] value, String result) {
+            this.value = value;
+            this.result = result;
+        }
+    }
+
+    @Parameterized.Parameters
+    public static Collection<TestCase[]> testCases() throws Exception {
+        List<TestCase[]> result = new ArrayList<>();
+
+        for (TestCase testCase: defaultTestCases())
+            result.add(new TestCase[] {testCase});
+
+        for (TestCase testCase: readTestCases("/SHA1/SHA1ShortMsg.rsp"))
+            result.add(new TestCase[] {testCase});
+
+        for (TestCase testCase: readTestCases("/SHA1/SHA1LongMsg.rsp"))
+            result.add(new TestCase[] {testCase});
+
+        return result;
+    }
+
+    private static TestCase[] defaultTestCases() {
+        TestCase[] result = new TestCase[10];
+
+        result[0] = new TestCase("abc".getBytes(), "a9993e364706816aba3e25717850c26c9cd0d89d");
+        result[1] = new TestCase("abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq".getBytes(), "84983e441c3bd26ebaae4aa1f95129e5e54670f1");
+        result[2] = new TestCase("abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu".getBytes(), "a49b2446a02c645bf419f995b67091253a04a259");
+        result[3] = new TestCase(StringUtils.leftPad("a", 1000000, "a").getBytes(), "34aa973cd4c4daa4f61eeb2bdbad27316534016f");
+        result[4] = new TestCase("".getBytes(), "da39a3ee5e6b4b0d3255bfef95601890afd80709");
+        result[5] = new TestCase(StringUtils.leftPad("1", 55, "1").getBytes(), "6b10c9ff4e3356b38d340918f62de8de87af9c6d");
+        result[6] = new TestCase(StringUtils.leftPad("1", 56, "1").getBytes(), "b8e49fda8515cc13b4d6bf80d97fca7a24732514");
+        result[7] = new TestCase(StringUtils.leftPad("1", 64, "1").getBytes(), "07a1a50a6273e6bc2eb94d647810cdc5b275b924");
+        result[8] = new TestCase(new byte[] {126, 61, 123, 62, 83, 87, 120, 102}, "fd0e6b0db87237f444fdd09a9540d47f32cd60ee");
+        result[9] = new TestCase("ßKÒ".getBytes(), "17d7a151c1b0f84ad647ea17ef9aae2109cee2bc");
+
+        return result;
+    }
+
+    private static byte[] fromHex2Bytes(String hexString) {
+        hexString = hexString.replaceAll("^(00)+", "");
+        byte[] bytes = new byte[hexString.length() / 2];
+        for (int i = 0; i < hexString.length(); i += 2) {
+            bytes[i / 2] = (byte) ((Character.digit(hexString.charAt(i), 16) << 4) + Character.digit(hexString.charAt(i + 1), 16));
+        }
+        return bytes;
+    }
+
+    private static Collection<? extends TestCase> readTestCases(String filePath) throws Exception {
+        List<String> contentList = Files.readAllLines(Path.of(SHA1NistTests.class.getResource(filePath).toURI()));
+        List<TestCase> result = new ArrayList<>();
+
+        for(int i = 0; i < contentList.size(); i++) {
+            if (! contentList.get(i).contains("Msg = ")) continue;
+
+            result.add(new TestCase(
+                    fromHex2Bytes(contentList.get(i++).split(" = ")[1]), // Msg = xxx
+                    contentList.get(i).split(" = ")[1].trim())   // MD = xxx
+            );
+        }
+
+        return result;
+    }
+
+    @Test
+    public void runNistTests() {
+        Assert.assertEquals(
+                String.format("Hashing message '%s' - string rep: %s", Arrays.toString(testCase.value), new String(testCase.value)),
+                testCase.result,
+                SHA1.hash(testCase.value)
+        );
+    }
+}
